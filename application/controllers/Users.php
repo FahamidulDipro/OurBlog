6a4463312776c7c1eb0882519c1d83237c4b67d5
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-02-08 01:32:17 +0600

    Making a core class and two Base Controllers
    Base controller User and Admin created.

---
+++ application/controllers/Users.php
@@ -0,0 +1,7 @@
+<?php
+    class Users extends My_controller{
+        public function index(){
+            echo "This is the index user ";
+        }
+    }
+?>
---
+++ application/core/My_controller.php
@@ -0,0 +1,9 @@
+<?php
+    class My_controller extends CI_controller{
+        // public function __construct()
+        // {
+        //   echo "Hey";
+        // }
+    
+    }
+?>
