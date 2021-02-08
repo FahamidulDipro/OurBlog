c396fde0a20c24ab5ce1d187e0ac7657e58789d1
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-02-08 20:18:06 +0600

    Login form validation

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,17 @@
 <?php
     class Admin extends My_controller{
         public function index(){
-            echo "This is the Admin user ";
+            // echo "This is the Admin user ";
+            $this->form_validation->set_rules('username','User Name','required|alpha');
+            $this->form_validation->set_rules('password','Password','required|max_length[12]');
+            if(  $this->form_validation->run()){
+                echo "Validation Successful";
+            }
+            else{
+          
+                $this->load->view('Users/articleList');
+            }
+          
         }
     }
 ?>
--- application/views/Users/articleList.php
+++ application/views/Users/articleList.php
@@ -2,32 +2,41 @@
 include "header.php";
 ?>
 <div class="container mt-3">
-<h1 style="margin-top:20px;">Admin Form</h1>
-<hr>
-<?php
+    <h1 style="margin-top:20px;">Admin Form</h1>
+    <hr>
+    <?php
     echo form_open('Admin/index');
-?>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Username</label>
+    ?>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Username</label>
+                <?php
+                echo form_input(['class' => 'form-control', 'placeholder' => 'Username', 'type' => 'text', 'name' => 'username']);
+                ?>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <?php
+                echo form_input(['class' => 'form-control', 'placeholder' => 'Password', 'type' => 'password', 'name' => 'password']);
+                ?>
+            </div>
+            <?php echo form_submit(['class' => 'btn btn-info', 'type' => 'submit', 'value' => 'Submit']); ?>
+            <?php echo form_reset(['class' => 'btn btn-secondary', 'type' => 'reset', 'value' => 'Reset']); ?>
+        </div>
+        <div class="col-lg-6 text-danger">
             <?php
-                echo form_input(['class'=>'form-control','placeholder'=>'Username','type'=>'text']);
+                echo form_error('username');
+                echo "<br><br><br>";
+            ?>
+            <?php
+                echo form_error('password');
+                echo "<br><br><br>";
             ?>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <?php
-                echo form_input(['class'=>'form-control','placeholder'=>'Password','type'=>'password']);
-            ?>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-            <?php
-                echo form_input(['class'=>'form-control','placeholder'=>'Confirm Password','type'=>'password']);
-            ?>
-        </div>
-      <?php echo form_submit(['class'=>'btn btn-info','type'=>'submit','value'=>'Submit']);?>
-      <?php echo form_reset(['class'=>'btn btn-secondary','type'=>'reset','value'=>'Reset']);?>
-    
+    </div>
+
+
 </div>
 
 <?php
